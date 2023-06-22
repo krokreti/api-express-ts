e5ac4ed8802bb2642707ed832de9deb0046c2424
davi <davipinheiro1993@gmail.com>
2023-06-22 13:49:46 -0300

    add cors, body and express with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import express from 'express';
+import body from 'body-parser';
+import cors from 'cors';
+
+const port = 3002;
+
+const app = express();
+
+app.use(body.json());
+app.use(express.json());
+app.use(cors);
+
+app.listen(port, () => {
+    console.log('Aplicação rodando na porta ' + port);
+})
